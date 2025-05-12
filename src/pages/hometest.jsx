83f9647841b6fe9000d6d003efdfe79fa7973667
melvin14320 <melvin.toure@outlook.fr>
2025-05-12 13:43:28 +0200

    creation des dossiers

---
+++ src/pages/hometest.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar1 from '../compotement/navbar1'
+
+function Test() {
+    return (
+        <>
+       <div>
+        <Navbar1 />
+    </div>
+        </>
+    )
+}
+
+export default Test;
